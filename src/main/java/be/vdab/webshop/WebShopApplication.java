c131090888bdc508ee70d734d364c1fdbcbe5286
VladimirZadorozhny <zadorozhny.vladimir@gmail.com>
2025-07-02 15:44:52 +0200

    Initial commit

---
+++ src/main/java/be/vdab/webshop/WebShopApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+
+}
