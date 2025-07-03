d9c045ec7f8828573ac564c04e2f6cd8610816f2
VladimirZadorozhny <zadorozhny.vladimir@gmail.com>
2025-07-03 22:59:03 +0200

    Remove @Autowired in test classes.

--- src/test/java/be/vdab/webshop/repositories/ProdgroupRepositoryTest.java
+++ src/test/java/be/vdab/webshop/repositories/ProdgroupRepositoryTest.java
@@ -1,31 +1,15 @@
 package be.vdab.webshop.repositories;
 
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.JdbcClientAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureJdbc;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.jdbc.Sql;
-
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.jdbc.JdbcTestUtils;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-//@DataJpaTest
-@SpringBootTest
-@Transactional
-@ExtendWith(SpringExtension.class)
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@AutoConfigureJdbc
+
+@DataJpaTest
 @Sql("/prodgroups.sql")
 class ProdgroupRepositoryTest {
     private static final String PRODGROUPS_TABLE = "prodgroups";
